18f2b45b72c7628d43f739bd15fdf446e6af88fb
Aashay Vanpal <arkhamknight.aashay@gmail.com>
2020-06-13 22:10:07 +0530

    POST Method and Plan div added

--- calc/views.py
+++ calc/views.py
@@ -56,8 +56,8 @@ def form(request):
     return render(request,'form.html')
 
 def result(request):
-    num1 = int(request.GET['num1'])
-    num2 = int(request.GET['num2'])
+    num1 = int(request.POST['num1'])
+    num2 = int(request.POST['num2'])
     result = num1+num2
     return render(request,'result.html',{'result':result})
 
